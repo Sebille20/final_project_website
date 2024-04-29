d9a2dc203056b835a3ef515ac9da1a214cea644b
Sebille20 <alice.optina20@gmail.com>
2024-04-30 00:11:24 +0800

    update script to suport one data, update css

--- script.js
+++ script.js
@@ -1,93 +1,80 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Fetch and display personal information
-    fetch('https://alice-portfolio.onrender.com/personal-info')
-        .then(response => response.json())
-        .then(data => {
-            const info = data[0];
-            document.getElementById('personal-info').innerHTML = `
-                <img src=${info.personalImg}>
-                <div class=name>${info.name}</div>
-                <div class="objective">
-                    <img class="objective-img" src=${info.objectiveIcon}>
-                    <div class="objective-text"> ${info.objective} </div>
-                </div>
-                <div class="phone">
-                    <img class="objective-img"
-                        src=${info.phoneIcon}>
-                    <div class="phone-text"> ${info.contactNo}</div>
-                </div>
-                <div class="email">
-                    <img class="objective-img"
-                        src=${info.emailIcon}>
-                    <div class="email-text">${info.email}</div>
-                </div>
-                <div class="email">
-                    <img class="objective-img"
-                        src=${info.homeIcon}>
-                    <div class="email-text">${info.address}</div>
-                </div>
-            `;
-        });
+    const fetchResumeData = async () => {
+        const response = await fetch('https://alice-portfolio.onrender.com/resume-data');
+        const data = await response.json();
+        return data;
+    };
 
-    // Fetch and display skills
-    fetch('https://alice-portfolio.onrender.com/skills')
-        .then(response => response.json())
-        .then(data => {
-            const skillsList = data.map(skill => `<li>${skill.description} - ${skill.expertiseLevel}</li>`).join('');
-            document.getElementById('skills').innerHTML = `
-                <h2>Skills</h2>
-                <ul>${skillsList}</ul>
-            `;
-        });
+    fetchResumeData().then((data) => {
+        // Retrieve the first item in the data array for personal information
+        const info = data.personalInfo;
 
-    // Fetch and display work experience
-    fetch('https://alice-portfolio.onrender.com/work-experience')
-        .then(response => response.json())
-        .then(data => {
-            const workExpList = data.map(exp => `
-                <div class="job-box">
-                    <div class="job-title"> ${exp.designation} @ ${exp.companyName}</div>
-                    <div class="job-date"> ${exp.year}</div>
-                    <div class="job-description">${exp.details}</div>
-                </div>
-            `).join('');
-            document.getElementById('work-experience').innerHTML = `
-                <h1 class="work-title">Work Experience</h1>
-                ${workExpList}
-            `;
-        });
+        // Fetch and display personal information
+        document.getElementById('personal-info').innerHTML = `
+            <img src=${info.personalImg} alt="Personal Image">
+            <div class="name">${info.name}</div>
+            <div class="objective">
+                <img class="icon" src=${info.objectiveIcon} alt="Objective Icon">
+                <div class="objective-text">${info.objective}</div>
+            </div>
+            <div class="phone">
+                <img class="icon" src=${info.phoneIcon} alt="Phone Icon">
+                <div class="phone-text">${info.contactNo}</div>
+            </div>
+            <div class="email">
+                <img class="icon" src=${info.emailIcon} alt="Email Icon">
+                <div class="email-text">${info.email}</div>
+            </div>
+            <div class="address">
+                <img class="icon" src=${info.homeIcon} alt="Home Icon">
+                <div class="address-text">${info.address}</div>
+            </div>
+        `;
 
-    // Fetch and display education
-    fetch('https://alice-portfolio.onrender.com/education')
-        .then(response => response.json())
-        .then(data => {
-            const educationList = data.map(edu => `
-                <div class="job-box">
-                    <div class="job-title"> ${edu.school}</div>
-                    <div class="job-date"> ${edu.year}</div>
-                    <div class="job-description"> ${edu.address}</div>
-                </div>
-            `).join('');
-            document.getElementById('education').innerHTML = `
-                <h1>Education</h1>
-                ${educationList}
-            `;
-        });
+        // Fetch and display skills
+        const skillsList = data.skills.map(skill => `<li>${skill.description} - ${skill.expertiseLevel}</li>`).join('');
+        document.getElementById('skills').innerHTML = `
+            <h2>Skills</h2>
+            <ul>${skillsList}</ul>
+        `;
 
-    // Fetch and display personal references
-    fetch('https://alice-portfolio.onrender.com/personal-references')
-        .then(response => response.json())
-        .then(data => {
-            const referencesList = data.map(ref => `
-                <div class="job-box">
-                    <div class="job-title"> ${ref.name}</div>
-                    <div class="job-date"> ${ref.relationship}</div>
-                    <div class="job-description">${ref.contactNumber}</div>
-                </div>
-            `).join('');
-            document.getElementById('personal-references').innerHTML = `
-                <h1>Personal References</h1>
-                ${referencesList}
-            `;
-        });
+        // Fetch and display work experience
+        const workExpList = data.workExperience.map(exp => `
+            <div class="job-box">
+                <div class="job-title">${exp.designation} @ ${exp.companyName}</div>
+                <div class="job-date">${exp.year}</div>
+                <div class="job-description">${exp.details}</div>
+            </div>
+        `).join('');
+        document.getElementById('work-experience').innerHTML = `
+            <h1 class="work-title">Work Experience</h1>
+            ${workExpList}
+        `;
+
+        // Fetch and display education
+        const educationList = data.education.map(edu => `
+            <div class="edu-box">
+                <div class="edu-title">${edu.school}</div>
+                <div class="edu-date">${edu.year}</div>
+                <div class="edu-address">${edu.address}</div>
+            </div>
+        `).join('');
+        document.getElementById('education').innerHTML = `
+            <h1>Education</h1>
+            ${educationList}
+        `;
+
+        // Fetch and display personal references
+        const referencesList = data.personalReferences.map(ref => `
+            <div class="ref-box">
+                <div class="ref-title">${ref.name}</div>
+                <div class="ref-date">${ref.relationship}</div>
+                <div class="ref-contact">${ref.contactNumber}</div>
+            </div>
+        `).join('');
+        document.getElementById('personal-references').innerHTML = `
+            <h1>Personal References</h1>
+            ${referencesList}
+        `;
+    });
 });
